d6ec651fef14fdd50a790e020541f7f9b78acb30
ATai2 <1114677814@qq.com>
2018-11-11 20:54:59 +0800

    Initial commit

---
+++ src/main/java/com/ppx/bipro/BiproApplication.java
@@ -0,0 +1,12 @@
+package com.ppx.bipro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiproApplication.class, args);
+    }
+}
