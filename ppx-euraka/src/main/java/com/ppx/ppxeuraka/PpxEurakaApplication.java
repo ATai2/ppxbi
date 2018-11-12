bca898251a09031889888424de2b55b78346143c
ATai2 <1114677814@qq.com>
2018-11-13 00:14:15 +0800

    add bi module

---
+++ ppx-euraka/src/main/java/com/ppx/ppxeuraka/PpxEurakaApplication.java
@@ -0,0 +1,12 @@
+package com.ppx.ppxeuraka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpxEurakaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpxEurakaApplication.class, args);
+    }
+}
