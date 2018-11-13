5eecbc751fc6a14072605ddbf02f244985703f71
ATai2 <1114677814@qq.com>
2018-11-13 19:25:54 +0800

    add bi module

---
+++ ppx-cloudadmin/src/main/java/com/ppx/ppxcloudadmin/PpxCloudadminApplication.java
@@ -0,0 +1,12 @@
+package com.ppx.ppxcloudadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpxCloudadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpxCloudadminApplication.class, args);
+    }
+}
