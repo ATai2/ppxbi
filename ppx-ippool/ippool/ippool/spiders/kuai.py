81ac0aa02ec6763bf90ad455e3ccd03aef0f1ad8
ATai2 <1114677814@qq.com>
2018-11-16 01:03:31 +0800

    ippool

--- ppx-ippool/ippool/ippool/spiders/kuai.py
+++ ppx-ippool/ippool/ippool/spiders/kuai.py
@@ -9,14 +9,11 @@ from ippool.items import IppoolItem
 class KuaiSpider(CrawlSpider):
     name = 'kuai'
     allowed_domains = ['kuaidaili.com']
-    start_urls = ['https://www.kuaidaili.com/free/inha/1/']
-    # start_urls = ['http://kuaidaili.com/']
-    rules = (
-        # 提取匹配 'item.php' 的链接并使用spider的parse_item方法进行分析
-        Rule(LinkExtractor(allow=('/free/inha/\d+/',)), callback='parse_link', follow=True),
-    )
+    start_urls = ['https://www.kuaidaili.com/free/inha/2/']
+    rules = (Rule(LinkExtractor(allow=('/free/inha/\d+/'),deny=('/tel:4000580638')), callback='parse_test', follow=True),)
 
-    def parse(self, response):
+    def parse_test(self, response):
+        print(response.url)
         for sel in response.xpath('//tbody/tr'):
             item = IppoolItem()
             item['ip'] = sel.xpath('td[@data-title="IP"]/text()').extract()[0]
@@ -28,6 +25,7 @@ class KuaiSpider(CrawlSpider):
             item['lastVeryfyTime'] = sel.xpath('td[@data-title="最后验证时间"]/text()').extract()[0]
             # print(item)
             yield item
+
     def parse_link(self, response):
         for sel in response.xpath('//tbody/tr'):
             item = IppoolItem()
