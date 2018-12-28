b092d2dca68e8d68e1d007c6967bd2cf3b389ff0
ATai2 <1114677814@qq.com>
2018-12-28 22:53:21 +0800

    docker compose

--- ppx-usermgt/src/main/java/com/ppx/ppxusermgt/controller/UserController.java
+++ ppx-usermgt/src/main/java/com/ppx/ppxusermgt/controller/UserController.java
@@ -2,13 +2,16 @@ package com.ppx.ppxusermgt.controller;
 
 import com.ppx.ppxusermgt.dao.UserDao;
 import com.ppx.ppxusermgt.entity.CommonResp;
+import com.ppx.ppxusermgt.entity.DemoModel;
 import com.ppx.ppxusermgt.entity.PpxUser;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import jdk.nashorn.internal.objects.annotations.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping(value = "/user")
@@ -41,5 +44,14 @@ public class UserController {
         CommonResp<String> res = new CommonResp<>();
         userDao.save(user);
         return res;
+    }
+
+    @RequestMapping("/demo2")
+    public void demo2(@RequestBody @Valid DemoModel demo, BindingResult result){
+        if(result.hasErrors()){
+            for (ObjectError error : result.getAllErrors()) {
+                System.out.println(error.getDefaultMessage());
+            }
+        }
     }
 }
