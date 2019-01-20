4288007273dd2b6f4f931457e5db340e8b24f4f3
ATai2 <1114677814@qq.com>
2019-01-20 23:08:37 +0800

    gateway

---
+++ ppx-order/src/main/java/com/ppx/ppxorder/PpxOrderApplication.java
@@ -0,0 +1,14 @@
+package com.ppx.ppxorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpxOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpxOrderApplication.class, args);
+    }
+
+}
+
