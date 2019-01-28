1951e23bc5e5d350bce7af8bd6925341ebe750d2
ATai2 <1114677814@qq.com>
2019-01-29 00:16:28 +0800

    activiti starter

---
+++ ppx-activiti/src/main/java/com/ppx/ppxactiviti/config/DataSourceConfig.java
@@ -0,0 +1,18 @@
+package com.ppx.ppxactiviti.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@ComponentScan
+@Configuration
+public class DataSourceConfig {
+    private Environment envirenment;
+//    @Bean
+//    public DataSource getDataSource(){
+//
+//    }
+
+
+}
