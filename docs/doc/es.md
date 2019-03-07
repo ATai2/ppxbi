8da4f8c62092ad7c0bcaea9689f4089361ec8d31
ATai2 <1114677814@qq.com>
2019-03-08 00:43:51 +0800

    tree

---
+++ docs/doc/es.md
@@ -0,0 +1,6 @@
+## elasticsearch
+
+垂直搜索，站内搜索
+
+全文检索，lucene  
+倒排索引
---
+++ ppx-vuetree/vuetree/src/components/HelloWorld.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <el-tree
+            :data="data2"
+            show-checkbox
+            default-expand-all
+            node-key="id"
+            ref="tree"
+            highlight-current
+            :props="defaultProps">
+    </el-tree>
+
+
+    <div class="buttons">
+      <el-button @click="getCheckedNodes">通过 node 获取</el-button>
+      <el-button @click="getCheckedKeys">通过 key 获取</el-button>
+      <el-button @click="setCheckedNodes">通过 node 设置</el-button>
+      <el-button @click="setCheckedKeys">通过 key 设置</el-button>
+      <el-button @click="resetChecked">清空</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+    methods: {
+        getCheckedNodes() {
+            console.log(this.$refs.tree.getCheckedNodes());
+        },
+        getCheckedKeys() {
+            console.log(this.$refs.tree.getCheckedKeys());
+        },
+        setCheckedNodes() {
+            this.$refs.tree.setCheckedNodes([{
+                id: 5,
+                label: '二级 2-1'
+            }, {
+                id: 9,
+                label: '三级 1-1-1'
+            }]);
+        },
+        setCheckedKeys() {
+            this.$refs.tree.setCheckedKeys([3]);
+        },
+        resetChecked() {
+            this.$refs.tree.setCheckedKeys([]);
+        }
+    },
+
+    data() {
+        return {
+            data2: [{
+                id: 1,
+                label: '一级 1',
+                children: [{
+                    id: 4,
+                    label: '二级 1-1',
+                    children: [{
+                        id: 9,
+                        label: '三级 1-1-1'
+                    }, {
+                        id: 10,
+                        label: '三级 1-1-2'
+                    }]
+                }]
+            }, {
+                id: 2,
+                label: '一级 2',
+                children: [{
+                    id: 5,
+                    label: '二级 2-1'
+                }, {
+                    id: 6,
+                    label: '二级 2-2'
+                }]
+            }, {
+                id: 3,
+                label: '一级 3',
+                children: [{
+                    id: 7,
+                    label: '二级 3-1'
+                }, {
+                    id: 8,
+                    label: '二级 3-2'
+                }]
+            }],
+            defaultProps: {
+                children: 'children',
+                label: 'label'
+            }
+        };
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
