4e3262668631870fe2c5f7429c89432070462fae
ATai2 <1114677814@qq.com>
2019-03-21 21:17:23 +0800

    add es js css

--- ppx-base/src/test/java/LeetcodeTest.java
+++ ppx-base/src/test/java/LeetcodeTest.java
@@ -17,17 +17,35 @@ public class LeetcodeTest {
         char[] list = value.toCharArray();
         char[] listreverse = new char[list.length];
 
-        for (int i = 0; i < list.length ; i++) {
-            listreverse[list.length - i-1] = list[i];
+        for (int i = 0; i < list.length; i++) {
+            listreverse[list.length - i - 1] = list[i];
         }
         String re = String.valueOf(listreverse);
         long parseLong = Long.parseLong(re);
-        if (parseLong > Integer.MAX_VALUE||parseLong<Integer.MIN_VALUE) {
+        if (parseLong > Integer.MAX_VALUE || parseLong < Integer.MIN_VALUE) {
             return 0;
         }
         res = (int) (flag ? parseLong : -parseLong);
         return res;
     }
 
+    public int reverse2(int x) {
+        long res = 0L;
+        while (x != 0) {
+            res = res * 10 + x % 10;
+            x = x / 10;
+            if ((res < Integer.MIN_VALUE) || (res > Integer.MAX_VALUE)) {
+                return 0;
+            }
+        }
+        return (int) res;
+    }
+
+    public int myAtoi(String str) {
+        String value=str.trim();
+        int begin,end;
+
+        return 0;
+    }
 
 }
