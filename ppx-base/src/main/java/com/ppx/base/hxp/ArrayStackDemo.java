b31e8915cf23369aff839970a57ed96a2a1af668
ATai2 <1114677814@qq.com>
2019-06-27 22:18:11 +0800

    clean

---
+++ ppx-base/src/main/java/com/ppx/base/hxp/ArrayStackDemo.java
@@ -0,0 +1,56 @@
+package com.ppx.base.hxp;
+
+public class ArrayStackDemo {
+    public static void main(String[] args) {
+        ArrayStack arrayStack=new ArrayStack(4);
+        arrayStack.list();
+    }
+}
+
+class ArrayStack {
+    private int maxSize;
+    private int[] stack;
+    private int top = -1;
+
+    public ArrayStack(int maxSize) {
+        this.maxSize = maxSize;
+        stack = new int[maxSize];
+    }
+
+    public boolean isFull() {
+        return top == maxSize - 1;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public void push(int value) {
+        if (isFull()) {
+            System.out.println("stack is full.");
+        }
+        top++;
+        stack[top] = value;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("stack is empty");
+            throw new RuntimeException();
+        }
+        int value = stack[top];
+        top--;
+        return value;
+    }
+
+    public void list(){
+        if (isEmpty()) {
+            System.out.println("empty");
+            return;
+        }
+        for (int i =top; i >=0; i++) {
+            System.out.println("item:" + stack[i]);
+
+        }
+    }
+}
