5558debaaf850b187037382e01f7c6562d8c81b1
ATai2 <1114677814@qq.com>
2019-07-20 06:30:14 +0800

    suanfa

---
+++ ppx-base/src/main/java/com/ppx/base/hxp/Queue8.java
@@ -0,0 +1,46 @@
+package com.ppx.base.hxp;
+
+public class Queue8 {
+    int max = 8;
+    int[] array = new int[max];
+
+    public static void main(String[] args) {
+        Queue8 queue8 = new Queue8();
+        queue8.check(0);
+    }
+
+    void check(int n) {
+        if (n == max) {
+            print();
+            return;
+        }
+        for (int i = 0; i < max; i++) {
+            array[n] = i;
+            if (judge(n)) {
+                check(n + 1);
+            }
+        }
+    }
+
+    /**
+     * judge if conflict
+     *
+     * @param n
+     * @return
+     */
+    boolean judge(int n) {
+        for (int i = 0; i < n; i++) {
+            if (array[i] == array[n] || Math.abs(n - i) == Math.abs(array[n] - array[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void print() {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "  ");
+        }
+        System.out.println();
+    }
+}
