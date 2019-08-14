8e73686eabf42b01910184471358d817f032cf0b
ATai2 <1114677814@qq.com>
2019-08-14 23:45:33 +0800

    test

--- builddocker.groovy
+++
@@ -1,9 +0,0 @@
-
-// get all dirs
-
-//File files = new File("D:\\code\\ppxbi")
-//def listFiles = files.listFiles()
-//listFiles.filter{s->
-//    ((File)s).isDirectory()
-//    println(s)
-//}
---
+++ ppx-admin/src/test/java/com/ppx/ppxadmin/PpxAdminApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ppx.ppxadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PpxAdminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
