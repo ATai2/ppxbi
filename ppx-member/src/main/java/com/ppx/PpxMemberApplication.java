ae18774c59a4b70b92e9d0febbfeeeb2f20d653b
ATai2 <1114677814@qq.com>
2019-08-24 10:24:37 +0800

    jms

--- ppx-member/src/main/java/com/ppx/PpxMemberApplication.java
+++ ppx-member/src/main/java/com/ppx/PpxMemberApplication.java
@@ -3,8 +3,10 @@ package com.ppx;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.jms.annotation.EnableJms;
 
+@EnableEurekaClient
 @EnableJms
 @SpringBootApplication
 @MapperScan("com.ppx.dao")
