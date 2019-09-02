4a07d499d3bfca2b4e2f0696a8f6449edb8f2c8b
ATai2 <1114677814@qq.com>
2019-09-03 00:09:30 +0800

    cd

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+echo 'start to build common module'
+
+cd ./ppx-common
+pwd
+mvn clean package
+mvn install
+#mvn com.spotify:docker-maven-plugin:1.1.1:build
+
+
+
+cd ..
+filelist=`ls ./`
+pattern="^ppx-"
+for file in $filelist
+do
+#  echo $file
+  if [[ $file =~ $pattern ]];  then
+    echo "right $file"
+
+  fi
+done
+
+funcDir(){
+  cd $1
+  if test -e "docker"
+    then echo "has docker dir"
+    mvn clean package
+    mvn com.spotify:docker-maven-plugin:1.1.1:build
+    mvn com.spotify:docker-maven-plugin:1.1.1:push
+
+  else
+    echo "$1 does not contain docker dir"
+    fi
+  cd ..
+  echo "  "
+}
