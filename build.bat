e60b54404034eb72df07a76d6a43979e7eb7743e
ATai2 <1114677814@qq.com>
2019-09-03 01:32:08 +0800

    cd

---
+++ build.bat
@@ -0,0 +1,36 @@
+echo 'start to build common module'
+funcDir(){
+  cd $1
+  if test -e "docker"
+    then echo "has docker dir"
+    mvn clean package
+    mvn com.spotify:docker-maven-plugin:1.1.1:build
+    mvn com.spotify:docker-maven-plugin:1.1.1:push
+
+  else
+    echo "$1 does not contain docker dir"
+    fi
+  cd ..
+  echo "  "
+}
+
+
+
+cd ./ppx-common
+pwd
+mvn clean package
+mvn install
+#mvn com.spotify:docker-maven-plugin:1.1.1:build
+cd ..
+filelist=`ls ./`
+pattern="^ppx-"
+for file in $filelist
+do
+#  echo $file
+  if [[ $file =~ $pattern ]];  then
+    echo "right $file"
+    funcDir $file
+  fi
+done
+
+
