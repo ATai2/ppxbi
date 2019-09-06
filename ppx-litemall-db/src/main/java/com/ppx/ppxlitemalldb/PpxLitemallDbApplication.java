0c5f461759724580e4d0d21fba9496c1bc6a5252
ATai2 <1114677814@qq.com>
2019-09-06 22:28:54 +0800

    add db mapper test

--- ppx-litemall-db/src/main/java/com/ppx/ppxlitemalldb/PpxLitemallDbApplication.java
+++ ppx-litemall-db/src/main/java/com/ppx/ppxlitemalldb/PpxLitemallDbApplication.java
@@ -1,9 +1,11 @@
 package com.ppx.ppxlitemalldb;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ppx.ppxlitemalldb")
+@MapperScan("com.ppx.ppxlitemalldb.dao")
 public class PpxLitemallDbApplication {
     public static void main(String[] args) {
         SpringApplication.run(PpxLitemallDbApplication.class, args);
