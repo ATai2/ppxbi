692e447888eae683c11e3953cd8cdfafd352758a
ATai2 <1114677814@qq.com>
2019-10-23 14:00:36 +0800

    spi

--- dubbo-consumer/src/main/java/com/ppx/dubbo/spi/BenzCar.java
+++ dubbo-consumer/src/main/java/com/ppx/dubbo/spi/BenzCar.java
@@ -2,6 +2,10 @@ package com.ppx.dubbo.spi;
 
 
 import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.extension.ExtensionLoader;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class BenzCar implements CarUrlInterface {
     private CarUrlInterface carUrlInterface;
@@ -13,6 +17,15 @@ public class BenzCar implements CarUrlInterface {
     @Override
     public void getSize(URL url) {
         System.out.println("benz");
+        carUrlInterface.getSize(url);
+    }
+
+    public static void main(String[] args) {
+        ExtensionLoader<CarUrlInterface> extensionLoader = ExtensionLoader.getExtensionLoader(CarUrlInterface.class);
+        Map<String,String> map=new HashMap<>();
+        map.put("car", "red");
+        URL url=new URL("","",1,map);
+        CarUrlInterface carUrlInterface = extensionLoader.getExtension("benz");
         carUrlInterface.getSize(url);
     }
 }
